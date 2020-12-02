6a6f421d5fb0834063651abcf356ae0aa07b0448
yhpp <angchen2@outlook.com>
2020-12-02 15:12:41 +0800

    '新增退出功能'

--- AddressBookManagementSystem/Main.cpp
+++ AddressBookManagementSystem/Main.cpp
@@ -14,7 +14,38 @@ void showMenu() {
 	cout << "*****************************" << endl;
 }
 int main() {
-	showMenu();
+
+	int select;
+
+	while (true)
+	{
+		showMenu();
+
+		cin >> select;
+		switch (select)
+		{
+		case 1: // 添加联系人
+			break;
+		case 2: // 显示联系人
+			break;
+		case 3: // 删除联系人
+			break;
+		case 4: // 查找联系人
+			break;
+		case 5: // 修改联系人
+			break;
+		case 6: // 清空联系人
+			break;
+		case 0: // 退出通讯录
+			cout << "你已退出" << endl;
+			system("pause");
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
+
 	system("pause");
 	return 0;
 }
